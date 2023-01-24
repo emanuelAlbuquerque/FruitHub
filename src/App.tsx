50f38f8ff8d670803442fce5e566107cfb7fd5b6
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2023-01-24 16:17:13 -0300

    :sparkles: feat: Criando a logica inicial para ordernar os itens na tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,46 @@
+import { Dollar } from "assets/Icons/Dollar";
+import { FilterIcon } from "assets/Icons/FilterIcon";
+import { Menu } from "assets/Icons/Menu";
+import { AppBar } from "components/AppBar";
+import { Button } from "components/Button";
+import { Search } from "components/Search";
+import { Sort } from "components/Sort";
+import { useEffect, useState } from "react";
+import pratos from 'Utils/itens.json'
+
+export interface combosProps{
+  nome: string
+  combo: string
+}
+
 
 export function App() {
+
+  const [combos, setItens] = useState<combosProps[]>([
+    ...pratos
+  ])
+  const [sort, setSort] = useState('')
+
+  console.log('redenrizou')
+
   return (
-    <h1>Ola mundo </h1>
+    <>
+      <Sort setSort={setSort} />
+      {
+        sort === '' || sort === 'All'
+          ?
+            combos.map((combo, i) => (
+              <p key={i}>
+                {combo.nome}
+              </p>
+            ))
+          : 
+            combos.filter((combo) => combo.combo === sort).map((combo, i) => (
+            <p key={i}>
+              {combo.nome}
+            </p>
+          ))
+      }
+    </>
   )
 }
