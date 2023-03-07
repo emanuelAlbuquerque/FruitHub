485a30077c8a849e717a4dcc22828f2f832433e2
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2023-03-07 12:02:57 -0300

    feat: Linkando o componente de rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,5 @@
-import { Dollar } from "assets/Icons/Dollar";
-import { FilterIcon } from "assets/Icons/FilterIcon";
-import { Menu } from "assets/Icons/Menu";
-import { AppBar } from "components/AppBar";
-import { Button } from "components/Button";
-import { Search } from "components/Search";
-import { Sort } from "components/Sort";
-import { useEffect, useState } from "react";
-import pratos from 'Utils/itens.json'
+import { RoutesAplication } from 'routes/routes'
 
-export interface combosProps{
-  nome: string
-  combo: string
-}
-
-
-export function App() {
-
-  const [combos, setItens] = useState<combosProps[]>([
-    ...pratos
-  ])
-  const [sort, setSort] = useState('')
-
-  console.log('redenrizou')
-
-  return (
-    <>
-      <Sort setSort={setSort} />
-      {
-        sort === '' || sort === 'All'
-          ?
-            combos.map((combo, i) => (
-              <p key={i}>
-                {combo.nome}
-              </p>
-            ))
-          : 
-            combos.filter((combo) => combo.combo === sort).map((combo, i) => (
-            <p key={i}>
-              {combo.nome}
-            </p>
-          ))
-      }
-    </>
-  )
+export const App = () => {
+  return <RoutesAplication />
 }
