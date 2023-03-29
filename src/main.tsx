1fd83b67da58cb7bfc13709dcb8fe73f92c5fd02
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2023-03-29 10:01:11 -0300

    build: Adicionando o react-leaflet

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from 'App'
 import { ThemeProvider } from 'styled-components'
 import { theme } from 'styles/Theme'
 import { GlobalStyle } from 'styles/GlobalStyle'
+import 'leaflet/dist/leaflet.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -11,5 +12,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <GlobalStyle />
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
