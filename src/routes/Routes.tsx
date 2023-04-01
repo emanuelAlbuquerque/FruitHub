194d0adcf9c48c98051246232d216bbd3bc76d14
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2023-04-01 13:34:26 -0300

    feat: Adicionando o restante das rotas do projeto

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,9 @@
+import { Bascket } from 'Pages/Bascket'
+import { DeliveryStatus } from 'Pages/DeliveryStatus'
+import { Home } from 'Pages/Home'
 import { Login } from 'Pages/Login'
+import { OrderComplete } from 'Pages/OrderComplete'
+import { Product } from 'Pages/Product'
 import { Register } from 'Pages/Register'
 import { Splash } from 'Pages/Splash'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -10,6 +15,11 @@ export const RoutesAplication = () => {
         <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home/:id" element={<Home />} />
+        <Route path="/addBascket/:id" element={<Product />} />
+        <Route path="/basckets/:id" element={<Bascket />} />
+        <Route path="/orderComplete" element={<OrderComplete />} />
+        <Route path="/deliveryStatus" element={<DeliveryStatus />} />
       </Routes>
     </BrowserRouter>
   )
